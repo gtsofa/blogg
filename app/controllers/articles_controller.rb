e99492702a87af7d8afcee7e2ea9a671679922a2
gtsofa <jlsnyule@yahoo.com>
2017-03-16 00:10:19 +0300

    adds data to the controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,6 +3,11 @@ class ArticlesController < ApplicationController
   end
 
   def create
-  render plain: params[:article].inspect
+  #render plain: params[:article].inspect
+  #@article = Article.new(params[:article])
+  @article = Article.new(params.require(:article).permit(:title, :text))
+
+  @article.save
+  redirect_to @article
 end
 end
